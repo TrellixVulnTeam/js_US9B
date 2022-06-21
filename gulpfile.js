5149ad868520c04142cd549150b52609b7fadf99
John Woo <elitle111.kha@gmail.com>
2022-06-22 01:11:12 +0300

    ok

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+
+
+global.app = {
+    path: path,
+    gulp: gulp
+}
+
+import { copy } from "./gulp/task.copy.js";
+
+gulp.task('default', copy);
